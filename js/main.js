bdc8a37a9dcbe8600a9d31f02a65ae5e7a6ba0b0
Ruzimurodov <ganisherruzimirodov@gmail.com>
2022-03-02 23:54:20 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".form__input");
+var elResult = document.querySelector(".form__span");
+var toq = [];
+var juft = [];
+
+
+
+elForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+
+    var elInputVal = Number(elInput.value.trim());
+
+    if(elInputVal % 2 == 0){
+        juft.unshift(elInputVal);
+        elResult.textContent = "Juft";
+    }else{
+        toq.unshift(elInputVal)
+        elResult.textContent = "Toq";
+    }
+    
+    elInput.value = ""
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
